b282ddc4bdf9af646bd37b28c211cce87fe16dc5
Wagyourtail <wagyourtail@wagyourtail.xyz>
2024-06-07 20:48:14 -0500

    gradle plugin

---
+++ src/main/kotlin/xyz/wagyourtail/asm/gradle/AnnotationASMPlugin.kt
@@ -0,0 +1,11 @@
+package xyz.wagyourtail.asm.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+abstract class AnnotationASMPlugin : Plugin<Project> {
+
+    override fun apply(target: Project) {
+    }
+
+}
---
+++ src/main/kotlin/xyz/wagyourtail/asm/gradle/task/AnnotationASMJar.kt
@@ -0,0 +1,46 @@
+@file:Suppress("LeakingThis")
+
+package xyz.wagyourtail.unimined.expect.task
+
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.TaskAction
+import org.gradle.jvm.tasks.Jar
+import xyz.wagyourtail.asm.PathProcessor
+import xyz.wagyourtail.asm.gradle.utils.openZipFileSystem
+import xyz.wagyourtail.unimined.expect.utils.FinalizeOnRead
+import xyz.wagyourtail.unimined.expect.utils.MustSet
+
+abstract class AnnotationASMJar : Jar() {
+
+    @get:InputFiles
+    var inputFiles: FileCollection by FinalizeOnRead(MustSet())
+
+    @TaskAction
+    fun doTransform() {
+        for (input in inputFiles) {
+            if (input.isDirectory) {
+                val output = temporaryDir.resolve(input.name + "-asm")
+                PathProcessor.process(input.toPath(), output.toPath())
+                from(output)
+            } else if (input.extension == "jar") {
+                val output = temporaryDir.resolve(input.nameWithoutExtension + "-expect-platform." + input.extension)
+                input.toPath().openZipFileSystem().use { inputFs ->
+                    output.toPath().openZipFileSystem(mapOf("create" to true)).use { outputFs ->
+                        PathProcessor.process(
+                            inputFs.getPath("/"),
+                            outputFs.getPath("/")
+                        )
+                    }
+                }
+                from(project.zipTree(output))
+            } else if (input.exists()) {
+                throw IllegalStateException("ExpectPlatformJar: $input is not a directory or jar file")
+
+            }
+        }
+
+        copy()
+    }
+
+}
